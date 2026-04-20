e3b76e97b6e73f41dbf38284d87becdd6d1e2e09
RupeshShinde101 <therhs101@gmail.com>
2026-04-20 15:43:01 +0530

    Change PWA id to bypass stuck WebAPK, new start_url with query param

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // DEPLOY_VERSION: Update this string on each deploy to bust caches automatically.
 // The browser detects byte-level changes to sw.js and triggers an update.
-const CACHE_NAME = 'workmate4u-v2026042002';
+const CACHE_NAME = 'workmate4u-v2026042003';
 const STATIC_ASSETS = [
   '/index.html',
   '/browse.html',

873bb6bf828054778b3da8174a7147c90dd5a4c5
RupeshShinde101 <therhs101@gmail.com>
2026-06-09 12:10:37 +0530

    chore: extend beta trial by 30 days (end date 2026-07-09)

--- backend/config.py
+++ backend/config.py
@@ -97,7 +97,7 @@ class Config:
     # ----------------------------------------------------------------
     TRIAL_ACTIVE = os.environ.get('TRIAL_ACTIVE', 'true').lower() == 'true'
     TRIAL_INVITE_CODE = os.environ.get('TRIAL_INVITE_CODE', 'WORKMATE100')  # Change in Railway env vars
-    TRIAL_END_DATE = os.environ.get('TRIAL_END_DATE', '2026-06-08')  # 30 days from launch
+    TRIAL_END_DATE = os.environ.get('TRIAL_END_DATE', '2026-07-09')  # Extended +30 days (2026-06-09)
     TRIAL_MAX_USERS = int(os.environ.get('TRIAL_MAX_USERS', 100))
     
     # Validate PostgreSQL URL scheme
